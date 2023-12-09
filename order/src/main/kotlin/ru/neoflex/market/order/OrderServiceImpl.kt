e8551c8f4f5b6e77b4d0abf10afcf77f1a37cace
Pavel Basmanov <pbasmanov@neoflex.ru>
2023-12-10 01:51:35 +0300

    add grpc

--- order/src/main/kotlin/ru/neoflex/market/order/OrderServiceImpl.kt
+++ order/src/main/kotlin/ru/neoflex/market/order/OrderServiceImpl.kt
@@ -1,15 +1,14 @@
 package ru.neoflex.market.order
 
-import io.camunda.zeebe.client.ZeebeClient
-import io.camunda.zeebe.client.api.response.ActivatedJob
-import io.camunda.zeebe.client.api.worker.JobClient
-import io.camunda.zeebe.client.api.worker.JobHandler
 import io.camunda.zeebe.spring.client.annotation.JobWorker
 import io.camunda.zeebe.spring.client.annotation.Variable
+import io.grpc.stub.StreamObserver
 import org.springframework.stereotype.Service
+import ru.neoflex.market.order.OrderServiceOuterClass.OrderRequest
+import ru.neoflex.market.order.OrderServiceOuterClass.OrderResponse
 
 @Service
-class OrderService {
+class OrderServiceImpl : OrderServiceGrpc.OrderServiceImplBase() {
 
     @JobWorker(type = "ors")
     fun fWorker(@Variable input: String): Map<String, Any> {
