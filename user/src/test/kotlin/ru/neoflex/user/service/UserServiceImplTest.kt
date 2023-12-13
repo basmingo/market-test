d984bf245c30ec7baf1053a24a1a992fe8f75692
Pavel Basmanov <pbasmanov@neoflex.ru>
2023-12-14 00:36:30 +0300

    reformat

--- user/src/test/kotlin/ru/neoflex/user/service/UserServiceImplTest.kt
+++ user/src/test/kotlin/ru/neoflex/user/service/UserServiceImplTest.kt
@@ -153,8 +153,8 @@ class UserServiceImplTest {
             val responseUserId: UUID = UUID.fromString(response.userId)
             val result: List<Boolean> = listOf(99.1, 0.1, 0.72, 0.07, 0.01, 0.1)
                 .map {
-                userServiceImpl.minusUserBalanceWorker(responseUserId, BigDecimal("$it"))
-            }
+                    userServiceImpl.minusUserBalanceWorker(responseUserId, BigDecimal("$it"))
+                }
 
             assert(result == listOf(true, true, true, true, true, false))
         }
