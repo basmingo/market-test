584b87db9b3c7cb65271c84cadeed68bd7d3196d
Pavel Basmanov <pbasmanov@neoflex.ru>
2024-01-14 18:03:45 +0300

    update host:port config

--- order/src/main/kotlin/ru/neoflex/market/order/service/worker/OrderActivityWorker.kt
+++ order/src/main/kotlin/ru/neoflex/market/order/service/worker/OrderActivityWorker.kt
@@ -3,8 +3,10 @@ package ru.neoflex.market.order.service.worker
 import io.temporal.client.WorkflowClient
 import io.temporal.client.WorkflowClientOptions
 import io.temporal.serviceclient.WorkflowServiceStubs
+import io.temporal.serviceclient.WorkflowServiceStubsOptions
 import io.temporal.worker.WorkerFactory
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import ru.neoflex.market.order.service.OrderActivityImpl
 import javax.annotation.PostConstruct
@@ -12,21 +14,29 @@ import javax.annotation.PostConstruct
 @Service
 class OrderActivityWorker {
 
+    @Value("\${temporal.host}")
+    lateinit var temporalHost: String
+
+    @Value("\${temporal.port}")
+    lateinit var temporalPort: String
+
     @Autowired
     lateinit var orderActivity: OrderActivityImpl
 
-    val client: WorkflowClient = WorkflowClient.newInstance(
-        WorkflowServiceStubs
-            .newLocalServiceStubs(),
+    @PostConstruct
+    fun workerStart() {
+        val options: WorkflowServiceStubsOptions = WorkflowServiceStubsOptions
+            .newBuilder()
+            .setTarget("$temporalHost:$temporalPort")
+            .build()
 
-        WorkflowClientOptions
+        val clientOptions: WorkflowClientOptions = WorkflowClientOptions
             .newBuilder()
             .setNamespace("default")
             .build()
-    )
 
-    @PostConstruct
-    fun workerStart() {
+        val client: WorkflowClient =
+            WorkflowClient.newInstance(WorkflowServiceStubs.newServiceStubs(options), clientOptions)
         val workerFactory = WorkerFactory.newInstance(client)
         val worker = workerFactory.newWorker("OrderServiceActivity")
         worker.registerActivitiesImplementations(orderActivity)
--- user/src/main/kotlin/ru/neoflex/market/user/service/worker/UserActivityWorker.kt
+++ user/src/main/kotlin/ru/neoflex/market/user/service/worker/UserActivityWorker.kt
@@ -3,8 +3,10 @@ package ru.neoflex.market.user.service.worker
 import io.temporal.client.WorkflowClient
 import io.temporal.client.WorkflowClientOptions
 import io.temporal.serviceclient.WorkflowServiceStubs
+import io.temporal.serviceclient.WorkflowServiceStubsOptions
 import io.temporal.worker.WorkerFactory
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import ru.neoflex.market.user.service.UserActivityImpl
 import javax.annotation.PostConstruct
@@ -12,21 +14,29 @@ import javax.annotation.PostConstruct
 @Service
 class UserActivityWorker {
 
+    @Value("\${temporal.host}")
+    lateinit var temporalHost: String
+
+    @Value("\${temporal.port}")
+    lateinit var temporalPort: String
+
     @Autowired
     lateinit var userActivity: UserActivityImpl
 
-    val client: WorkflowClient = WorkflowClient.newInstance(
-        WorkflowServiceStubs
-            .newLocalServiceStubs(),
+    @PostConstruct
+    fun workerStart() {
+        val options: WorkflowServiceStubsOptions = WorkflowServiceStubsOptions
+            .newBuilder()
+            .setTarget("$temporalHost:$temporalPort")
+            .build()
 
-        WorkflowClientOptions
+        val clientOptions: WorkflowClientOptions = WorkflowClientOptions
             .newBuilder()
             .setNamespace("default")
             .build()
-    )
 
-    @PostConstruct
-    fun workerStart() {
+        val client: WorkflowClient =
+            WorkflowClient.newInstance(WorkflowServiceStubs.newServiceStubs(options), clientOptions)
         val workerFactory = WorkerFactory.newInstance(client)
         val worker = workerFactory.newWorker("UserServiceActivity")
         worker.registerActivitiesImplementations(userActivity)
