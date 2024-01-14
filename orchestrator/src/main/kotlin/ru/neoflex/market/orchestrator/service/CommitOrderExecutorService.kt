091d843b6d2511b41405317fd1ee4e51ddbe60be
Pavel Basmanov <pbasmanov@neoflex.ru>
2024-01-14 17:26:23 +0300

    update host:port config

--- orchestrator/src/main/kotlin/ru/neoflex/market/orchestrator/service/CommitOrderExecutorService.kt
+++ orchestrator/src/main/kotlin/ru/neoflex/market/orchestrator/service/CommitOrderExecutorService.kt
@@ -4,7 +4,9 @@ import io.temporal.client.WorkflowClient
 import io.temporal.client.WorkflowClientOptions
 import io.temporal.client.WorkflowOptions
 import io.temporal.serviceclient.WorkflowServiceStubs
+import io.temporal.serviceclient.WorkflowServiceStubsOptions
 import net.devh.boot.grpc.server.service.GrpcService
+import org.springframework.beans.factory.annotation.Value
 import ru.neoflex.market.orchestrator.workflow.CommitOrder
 import ru.neoflex.market.order.OrchestratorServiceGrpcKt
 import ru.neoflex.market.order.OrhcestratorService.*
@@ -13,22 +15,33 @@ import java.util.*
 
 @GrpcService
 class CommitOrderExecutorService : OrchestratorServiceGrpcKt.OrchestratorServiceCoroutineImplBase() {
-    override suspend fun commitOrder(request: CommitOrderRequest): CommitOrderResponse {
-        val client: WorkflowClient = WorkflowClient.newInstance(
-            WorkflowServiceStubs
-                .newLocalServiceStubs(),
-            WorkflowClientOptions
-                .newBuilder()
-                .setNamespace("default")
-                .build()
-        )
 
-        val options: WorkflowOptions = WorkflowOptions
+    @Value("\${temporal.host}")
+    lateinit var temporalHost: String
+
+    @Value("\${temporal.port}")
+    lateinit var temporalPort: String
+
+    override suspend fun commitOrder(request: CommitOrderRequest): CommitOrderResponse {
+        val options: WorkflowServiceStubsOptions = WorkflowServiceStubsOptions
+            .newBuilder()
+            .setTarget("$temporalHost:$temporalPort")
+            .build()
+
+        val clientOptions: WorkflowClientOptions = WorkflowClientOptions
+            .newBuilder()
+            .setNamespace("default")
+            .build()
+
+        val client: WorkflowClient =
+            WorkflowClient.newInstance(WorkflowServiceStubs.newServiceStubs(options), clientOptions)
+
+        val workflowOptions: WorkflowOptions = WorkflowOptions
             .newBuilder()
             .setTaskQueue("CommitOrderWorkflow")
             .build()
 
-        val workflow = client.newWorkflowStub(CommitOrder::class.java, options)
+        val workflow = client.newWorkflowStub(CommitOrder::class.java, workflowOptions)
         workflow.execute(UUID.fromString(request.userId), BigDecimal(request.amount))
         return CommitOrderResponse.newBuilder().build()
     }
--- orchestrator/src/main/kotlin/ru/neoflex/market/orchestrator/workflow/CommitOrderWorker.kt
+++ orchestrator/src/main/kotlin/ru/neoflex/market/orchestrator/workflow/CommitOrderWorker.kt
@@ -3,24 +3,36 @@ package ru.neoflex.market.orchestrator.workflow
 import io.temporal.client.WorkflowClient
 import io.temporal.client.WorkflowClientOptions
 import io.temporal.serviceclient.WorkflowServiceStubs
+import io.temporal.serviceclient.WorkflowServiceStubsOptions
 import io.temporal.worker.WorkerFactory
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import javax.annotation.PostConstruct
 
 @Service
 class CommitOrderWorker {
-    val client: WorkflowClient = WorkflowClient.newInstance(
-        WorkflowServiceStubs
-            .newLocalServiceStubs(),
 
-        WorkflowClientOptions
-            .newBuilder()
-            .setNamespace("default")
-            .build()
-    )
+    @Value("\${temporal.host}")
+    lateinit var temporalHost: String
+
+    @Value("\${temporal.port}")
+    lateinit var temporalPort: String
 
     @PostConstruct
     fun workerStart() {
+        val options: WorkflowServiceStubsOptions = WorkflowServiceStubsOptions
+            .newBuilder()
+            .setTarget("$temporalHost:$temporalPort")
+            .build()
+
+        val clientOptions: WorkflowClientOptions = WorkflowClientOptions
+            .newBuilder()
+            .setNamespace("default")
+            .build()
+
+        val client: WorkflowClient =
+            WorkflowClient.newInstance(WorkflowServiceStubs.newServiceStubs(options), clientOptions)
+
         val workerFactory = WorkerFactory.newInstance(client)
         val worker = workerFactory.newWorker("CommitOrderWorkflow")
         worker.registerWorkflowImplementationTypes(CommitOrderImpl::class.java)
